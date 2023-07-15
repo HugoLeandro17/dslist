b32f2713b1759f27eda6dc28b71a0bef9a255d73
Hugo Leandro <62100014+HugoLeandro17@users.noreply.github.com>
2023-07-15 10:57:06 +0100

    Get Game by id

---
+++ src/main/java/com/hugoleandro/dslist/dto/GameAllDTO.java
@@ -0,0 +1,67 @@
+package com.hugoleandro.dslist.dto;
+
+import com.hugoleandro.dslist.models.Game;
+
+public class GameAllDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameAllDTO() {}
+
+    public GameAllDTO(Game entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.year = entity.getYear();
+        this.genre = entity.getGenre();
+        this.platforms = entity.getPlatforms();
+        this.score = entity.getScore();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+        this.longDescription = entity.getLongDescription();
+
+        //BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+}
--- src/main/java/com/hugoleandro/dslist/services/GameService.java
+++ src/main/java/com/hugoleandro/dslist/services/GameService.java
@@ -1,22 +1,38 @@
 package com.hugoleandro.dslist.services;
 
+import com.hugoleandro.dslist.dto.GameAllDTO;
 import com.hugoleandro.dslist.dto.GameDTO;
 import com.hugoleandro.dslist.models.Game;
 import com.hugoleandro.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameDTO> findAll() {
         List<Game> listAllGames = gameRepository.findAll();
 
         return listAllGames.stream().map(game -> new GameDTO(game)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameAllDTO findById(Long id) {
+        Optional<Game> game = gameRepository.findById(id);
+
+        if (game.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Game with id %d not found", id));
+
+        return new GameAllDTO(game.get());
     }
 }
